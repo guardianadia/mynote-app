0a5d72b164ed134d6faf98d9a07070e210b7ee46
guardianadia <guardia.nadia2026@gmail.com>
2026-05-03 18:56:00 -0400

    Update supabase_config.dart

--- lib/services/supabase_config.dart
+++ lib/services/supabase_config.dart
@@ -1,5 +1,5 @@
 class SupabaseConfig {
-  static const String url = 'https://loaallkwmwgqlxhndwrf.supabase.co';
+  static const String url = 'YOUR_SUPABASE_URL';
   static const String anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvYWFsbGt3bXdncWx4aG5kd3JmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwNTM2MDgsImV4cCI6MjA4OTYyOTYwOH0.75Vf0xOH4eHYufPZm24U5M0buXKaxkzlPSKBCIlhvgk';
+      'YOUR_SUPABASE_KEY';
 }
